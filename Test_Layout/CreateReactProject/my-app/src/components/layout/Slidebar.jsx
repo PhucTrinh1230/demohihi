52810c6539a89174e0777b795364bf3b024697db
Admin <trinhphunghongphuc15012000@gmail.com>
2024-05-29 10:37:49 +0700

    init project

--- Test_Layout/CreateReactProject/my-app/src/components/layout/Slidebar.jsx
+++ Test_Layout/CreateReactProject/my-app/src/components/layout/Slidebar.jsx
@@ -1,6 +1,6 @@
 import './Slidebar.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 const Slidebar = () => {
     return (
         <div className='container'>
@@ -23,29 +23,29 @@ const Slidebar = () => {
                                 <li className="list-group-item"><strong>GIỚI THIỆU</strong>
                                 </li>
                                 <nav>
-                                <Link to='/'>Vi Sao nen mua game ban quyen tai Steam</Link>
-                                <Link to='/support2'>Khong gian lam viec</Link>
-                                <Link to='/'>Vi Sao nen mua game ban quyen tai Steam</Link>
-                                <Link to='/'>Khong gian lam viec</Link>
-                                <Link to='/'>Vi Sao nen mua game ban quyen tai Steam</Link>
-                                <Link to='/'>Khong gian lam viec</Link>
+                                <NavLink to='/'>Lý do nên mua game bản quyền</NavLink>
+                                <NavLink to='/support2'>Giới thiệu Steam Shop</NavLink>
+                                <NavLink to='/support3'>Hệ thống Fanpage chính thức </NavLink>
+                                <NavLink to='/support4'>Chính sách VIP</NavLink>
+                                <NavLink to='/support5'>Điều khoản dịch vụ</NavLink>
+                                <NavLink to='/support6'>Chính sách bảo mật</NavLink>
                                 </nav>
                               
                                 <li className="list-group-item"><strong>HƯỚNG DẪN MUA HÀNG</strong>
                                 </li>
                                <nav>
-                                <Link to='/'>Khong gian lam viec</Link>
-                                <Link to='/'>Vi Sao nen mua game ban quyen tai Steam</Link>
-                                <Link to='/'>Khong gian lam viec</Link>
+                                <NavLink to='/support7'>Hướng dẫn tạo tài khoản</NavLink>
+                                <NavLink to='/support8'>Hướng dẫn nạp tiền</NavLink>
+                                <NavLink to='/support9'>Hướng dẫn thanh toán</NavLink>
                                 </nav>
                                 <li className="list-group-item"><strong>BẢO HÀNH</strong>
                                 </li>
                                 <nav>
-                                <Link to='/'>Khong gian lam viec</Link>
+                                <NavLink to='/support10'>Thông tin bảo hành chung</NavLink>
                                 </nav>
                                 <li className="list-group-item"><strong>CÂU HỎI THƯỜNG GẶP</strong>
                                 </li>
-                                <nav> <Link to='/'>Khong gian lam viec</Link></nav>
+                                <nav> <NavLink to='/support11'>Hướng dẫn lấy lại account</NavLink></nav>
 
 
 
